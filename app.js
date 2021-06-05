6d0071ea1a5cfc6fd49a37c021ca718a8c75cd4e
juju2181 <anishshilpakar8@gmail.com>
2021-06-05 11:36:54 +0545

    Conway's Game Of Life V1.0

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-/* Selectors from Html*/
+/* --- Selectors from Html --- */
+// get the div for world grid
 const world = document.querySelector('#world');
+// get the button to start and stop simulation
 const startStopButton = document.querySelector('#startStopBtn');
+// get the button to reset the world
 const resetButton = document.querySelector('.reset-btn');
+// get the button to generate random cells
 const randomButton = document.querySelector('.random-btn');
+// get the h3 that displays the alive count
 const aliveCount = document.querySelector('.alive-count');
+// get the h3 that displays the time of simulation
 const stopwatch = document.querySelector('.stopwatch');
+// get the mPopup
+const modalBox = document.querySelector('#modalBox');
+// get the btn that will open the modal
+const modalPopupButton = document.querySelector("#modal-popup-btn");
+// get the close btn of modal
+const closeButton = document.querySelector(".close-btn");
+// modal footer for date
+const modalFooter = document.querySelector("#modal-footer");
+
 /* -- Variables -- */
+//  we can also directly specifying no of rows and cols
 const resolution = 10;
 const width = 400;
 const height = 400;
@@ -16,10 +32,10 @@ const columns = height / resolution;
 // ? We need to create 2d arrays from these 1D arrays 
 let currentGeneration = [rows];
 let nextGeneration = [rows];
-console.log('1D', currentGeneration);
+// console.log('1D', currentGeneration);
 // ? variables for start stop button
 let hasGameStarted = false; //set it to true when game starts
-let timer;  //to control evolutions
+let timer; //to control evolutions
 let evolutionSpeed = 1000; //each generation will change in half second
 // ? variables for stopwatch
 let hours = 0;
@@ -35,7 +51,7 @@ function startStopwatch() {
     if (isTimeStopped === true) {
         isTimeStopped = false;
         updateTime();
-    } 
+    }
 }
 
 function stopStopwatch() {
@@ -77,7 +93,7 @@ function updateTime() {
         setTimeout(updateTime, 1000);
     }
 }
-
+// ? function to reset time
 function resetTime() {
     hours = 0;
     minutes = 0;
@@ -90,7 +106,7 @@ function resetTime() {
 
 //? function to create the 2D array from the 1D array
 function create2DGenerationArrays() {
-    for (let i = 0; i < rows; i++){
+    for (let i = 0; i < rows; i++) {
         // for creating 2D array each row will have array of columns
         currentGeneration[i] = new Array(columns);
         nextGeneration[i] = new Array(columns);
@@ -99,29 +115,29 @@ function create2DGenerationArrays() {
 }
 // ? function to initialize the 2D array
 function init2DGenerationArrays() {
-    for (let i = 0; i < rows; i++){
-        for (let j = 0; j < columns; j++){
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
             currentGeneration[i][j] = 0;
             nextGeneration[i][j] = 0;
         }
     }
-    console.log(currentGeneration);
+    // console.log(currentGeneration);
 }
 
 //? function to create the world grid
-function createWorld(){
+function createWorld() {
     //* we will create a table with rows and columns
     let table = document.createElement('table');
     table.setAttribute('id', 'worldgrid');
     // creating the rows and columns using for loop
-    for (let i = 0; i < rows; i++){
+    for (let i = 0; i < rows; i++) {
         //* create table row
         let table_row = document.createElement('tr');
-        for (let j = 0; j < columns; j++){
+        for (let j = 0; j < columns; j++) {
             //* create table cell and add its attribute
             let table_cell = document.createElement('td');
             table_cell.setAttribute('id', `${i}_${j}`);
-            table_cell.setAttribute('class','dead');
+            table_cell.setAttribute('class', 'dead');
             // table_cell.classList.add('dead');
             table_cell.addEventListener('click', toggleCellLife);
             table_row.appendChild(table_cell);
@@ -146,30 +162,33 @@ function getAliveCount() {
 
 
 // ? function to togggle life of cell
-function toggleCellLife(){
+function toggleCellLife() {
     // As the id for each cell is its position seperated by _ so by splitting it in _ we get the required location of cell in form of array i-e location = [row,column]
     let location = this.id.split("_");
     // getting the required row and column in form of number
     let row = Number(location[0]); //? get i position
-    let column = Number(location[1]);//? get j position
-    console.log(row,column);
+    let column = Number(location[1]); //? get j position
+    // console.log(row, column);
     // ? Toggle the cell for alive or dead
     //? if active change to dead
     let no_of_alive_cells;
-    if (this.className === 'alive') {
-        this.setAttribute('class', 'dead');
-        currentGeneration[row][column] = 0;
-        console.log(`${this.id} Cell is dead`);
-        no_of_alive_cells = getAliveCount();
-        aliveCount.innerText = no_of_alive_cells;
-    } else {
-        this.setAttribute('class', 'alive');
-        currentGeneration[row][column] = 1;
-        console.log(`${this.id} Cell is Alive`);
-        no_of_alive_cells = getAliveCount();
-        aliveCount.innerText = no_of_alive_cells;
+    if (hasGameStarted === false) {
+        if (this.className === 'alive') {
+            this.setAttribute('class', 'dead');
+            currentGeneration[row][column] = 0;
+            console.log(`${this.id} Cell is dead`);
+            no_of_alive_cells = getAliveCount();
+            aliveCount.innerText = no_of_alive_cells;
+        } else {
+            this.setAttribute('class', 'alive');
+            currentGeneration[row][column] = 1;
+            console.log(`${this.id} Cell is Alive`);
+            no_of_alive_cells = getAliveCount();
+            aliveCount.innerText = no_of_alive_cells;
+        }
     }
     
+
     // here setAttrribute is better than using classList.add as we need to manually remove the dead class again
     // this.classList.contains('alive') ? this.classList.remove('alive') : this.classList.add('alive');
 }
@@ -244,7 +263,7 @@ function getNeighborCount(row, column) {
 }
 
 
-// ? function to create next generation of cells
+// ? function to create next generation of cells using rules
 function createNextGeneration() {
     // looping through the 2d array to create next gen
     for (row in currentGeneration) {
@@ -253,7 +272,7 @@ function createNextGeneration() {
             console.log(`Neighbours for ${row}_${column} = `, neighbors_count);
             // checking the rules for game of life
             // if alive
-            if(currentGeneration[row][column] === 1){
+            if (currentGeneration[row][column] === 1) {
                 if (neighbors_count < 2) {
                     // * underpopulation
                     nextGeneration[row][column] = 0;
@@ -296,7 +315,7 @@ function updateWorld() {
     aliveCount.innerText = no_of_alive_cells;
     if (no_of_alive_cells === 0) {
         changeGameState();
-        
+
     }
     for (row in currentGeneration) {
         for (column in currentGeneration[row]) {
@@ -321,18 +340,17 @@ function evolve() {
     // here we will call the evolve function recursively in certain time determined by evolution speed using setTimeout
     if (hasGameStarted) {
         timer = setTimeout(evolve, evolutionSpeed);
-        console.log(`Timer: ${timer}`);
+        // console.log(`Timer: ${timer}`);
     }
 }
 
 
 //? function to start and stop the evolution process using timer
 function changeGameState() {
-    
+
     if (!hasGameStarted) {
         let aliveCellsResult = getAliveCount();
-        console.log(aliveCellsResult);
-        // return;
+        // console.log(aliveCellsResult);
         if (aliveCellsResult < 1) {
             alert("No cells are currently alive!!\nAtleast make atleast one cell alive or generate random cells before starting");
             return;
@@ -348,18 +366,18 @@ function changeGameState() {
             startStopwatch();
             evolve();
         }
-        } else {
-            hasGameStarted = false;
-            if (startStopButton.classList.contains('stop-btn')) {
-                startStopButton.classList.replace('stop-btn', 'start-btn');
-            }
-            // startStopButton.classList.add('stop-btn');
-            startStopButton.innerHTML = '<span><i class="fa fa-play"></i></span> Start Simulation';
-            randomButton.style.display = "block";
-            clearTimeout(timer);
-            stopStopwatch();
+    } else {
+        hasGameStarted = false;
+        if (startStopButton.classList.contains('stop-btn')) {
+            startStopButton.classList.replace('stop-btn', 'start-btn');
         }
-    
+        // startStopButton.classList.add('stop-btn');
+        startStopButton.innerHTML = '<span><i class="fa fa-play"></i></span> Start Simulation';
+        randomButton.style.display = "block";
+        clearTimeout(timer);
+        stopStopwatch();
+    }
+
 }
 
 // ? function to reset the world
@@ -369,11 +387,11 @@ function resetWorld() {
 }
 
 // ? function to generate random pattern
-function generateRandomPattern(no_of_cells=1) {
+function generateRandomPattern(no_of_cells = 1) {
     for (let i = 0; i < no_of_cells; i++) {
         // getting the required row and column in form of number
         let row = Math.floor(Math.random() * rows); //? get i position
-        let column = Math.floor(Math.random() * columns);//? get j position
+        let column = Math.floor(Math.random() * columns); //? get j position
         // console.log(row,column);
         let randomCell = document.getElementById(`${row}_${column}`);
         randomCell.setAttribute('class', 'alive');
@@ -384,13 +402,45 @@ function generateRandomPattern(no_of_cells=1) {
     }
 }
 
+//* functions for modal
+function displayModal() {
+    modalBox.style.display = "flex";
+}
+
+function closeModal() {
+    modalBox.style.display = "none";
+}
+
 /* --  DOM functions --  */
 window.onload = () => {
     createWorld();
     create2DGenerationArrays();
     init2DGenerationArrays();
+    displayModal();
 }
 
+// * event listeners for the buttons
 startStopButton.addEventListener("click", changeGameState);
 resetButton.addEventListener("click", resetWorld);
-randomButton.addEventListener("click", () => { generateRandomPattern(50); });
+randomButton.addEventListener("click", () => {
+    generateRandomPattern(50);
+});
+
+// * event listeners for modal
+// add date to modal
+let currentDate = new Date();
+modalFooter.innerHTML = `<span>&copy;</span><a href='https://anish-shilpakar.com.np/'>Anish_Shilpakar</a>${currentDate.getFullYear()}`;
+// display the modal
+modalPopupButton.addEventListener("click", () => {
+    if (!hasGameStarted) {
+            displayModal();
+    } 
+});
+// close the modal
+closeButton.addEventListener("click", closeModal);
+// close when clicking outside of modal
+window.addEventListener("click", (e) => {
+    if (e.target == modalBox) {
+        closeModal();
+    }
+});
